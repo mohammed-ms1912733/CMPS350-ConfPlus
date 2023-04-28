39e06f5bb3002fc43a93b739edd4ac9c55955b54
Mohammed Salah <ms1912733@qu.edu.qa>
2023-04-28 21:31:54 +0300

    Friday Changes

--- confplus-app/public/script/index.js
+++ confplus-app/public/script/index.js
@@ -16,14 +16,41 @@ const password = document.querySelector("#password");
 //login button
 const loginBtn = document.querySelector("#loginButton");
 
+
 // login validation and redirecting the user to the panel
 loginBtn.addEventListener("click", async (e) => {
     e.preventDefault();
+    loadPanel()
+    // const users = await getUsers()
+    // if (users.find(user => user.email === email.value && user.password === password.value)) {
+    //     window.location.href = "./panel.html"
+    // }
+    // else {
+    //     alert("Invalid email or password")
+    // }
+    
+});
+
+// userLogo.innerHTML = `<img src="./images/organizerFemaleLogo.png" alt="user">`
+
+async function loadPanel() {
     const users = await getUsers()
-    if (users.find(user => user.email === email.value && user.password === password.value)) {
-        window.location.href = "../organizerPanel.html"
+    const user = users.find(user => user.email === email.value && user.password === password.value)
+    if (user) {
+        window.location.href = "./panel.html"
+        const userName = document.querySelector(".userName");
+        userName.innerHTML = `${user.firstName} ${user.lastName}`
+        console.log(user)
     }
     else {
         alert("Invalid email or password")
     }
-});
+
+    // const userLogo = document.querySelector(".userPicContainer");
+    // const userLogo = document.querySelector("#userPicContainer");
+    // userLogo.innerHTML = `hello`
+
+    // const userName = document.querySelector(".userName");
+    // alert(userName)
+}
+
